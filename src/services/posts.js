cab1cddb01bc279378af5dc1940152b9db48c703
chyla95 <chyla95@gmail.com>
2023-02-26 07:31:40 +0100

    Store mock data in localstorage

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,57 +4,69 @@ const delay = async (timeout) => {
 };
 
 const getNextId = () => {
-  const nextId = POSTS_MOCK[POSTS_MOCK.length - 1].id + 1;
+  const data = JSON.parse(localStorage.getItem("posts-mock"));
+
+  const nextId = data[data.length - 1].id + 1;
   return nextId;
 };
 
-// Mock data
-let POSTS_MOCK = [
-  {
-    id: 0,
-    author: "Steven O",
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 1,
-    author: "Teri M",
-    message:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    id: 2,
-    author: "Margaret M",
-    message:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  },
-  {
-    id: 3,
-    author: "Malvin S",
-    message:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  },
-];
+const setupMockData = () => {
+  // Mock data
+  let POSTS_MOCKS = [
+    {
+      id: 0,
+      author: "Steven O",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 1,
+      author: "Teri M",
+      message:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      id: 2,
+      author: "Margaret M",
+      message:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+    {
+      id: 3,
+      author: "Malvin S",
+      message:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ];
+
+  const data = localStorage.getItem("posts-mock");
+  if (!data) localStorage.setItem("posts-mock", JSON.stringify(POSTS_MOCKS));
+};
+setupMockData();
 
 // Mock service functions
 export const getPosts = async () => {
   await delay(1000); // 1s delay, to simulate real HTTP request
 
   console.log(`Service "getPosts" triggered!`);
-  return POSTS_MOCK;
+  const data = JSON.parse(localStorage.getItem("posts-mock"));
+  return data;
 };
 
 export const getPost = async (id) => {
   await delay(1000); // 1s delay, to simulate real HTTP request
 
   console.log(`Service "getPost" triggered!`);
-  return POSTS_MOCK.find((p) => p.id === id);
+  const data = JSON.parse(localStorage.getItem("posts-mock"));
+  return data.find((p) => p.id === id);
 };
 
 export const addPost = async (post) => {
   await delay(1000); // 1s delay, to simulate real HTTP request
 
   console.log(`Service "addPost" triggered!`);
+  const data = JSON.parse(localStorage.getItem("posts-mock"));
   const formattedPost = { id: getNextId(), ...post };
-  POSTS_MOCK.push(formattedPost);
+  localStorage.setItem("posts-mock", JSON.stringify([...data, formattedPost]));
+
   return formattedPost;
 };
