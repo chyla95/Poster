78bceae428973601191364d26a76588b5f6f1916
chyla95 <chyla95@gmail.com>
2023-02-26 16:25:29 +0100

    Move some components from Posts to PostsPage

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,35 +1,9 @@
-import { useState } from "react";
-import { MdPostAdd } from "react-icons/md";
-import { useSelector } from "react-redux";
-import AddPostModal from "../General/Overlays/Modals/AddPostModal";
 import Post from "./Post";
 import styles from "./Posts.module.css";
 
 const Posts = (props) => {
-  const { token } = useSelector((state) => state.auth);
-  const [isAddPostModalVisible, setIsAddPostModalVisible] = useState(false);
-
-  const showAddPostModal = (event) => {
-    setIsAddPostModalVisible(true);
-  };
-  const hideAddPostModal = async (event) => {
-    setIsAddPostModalVisible(false);
-  };
-
   return (
     <>
-      <AddPostModal
-        isVisible={isAddPostModalVisible}
-        hideAddPostModal={hideAddPostModal}
-      />
-
-      {token && (
-        <button className={styles["button"]} onClick={showAddPostModal}>
-          <MdPostAdd size={18} />
-          Add Post
-        </button>
-      )}
-
       <ul className={`${styles["posts"]}`}>
         {props.posts.map((p) => (
           <Post key={p.id} post={p} />
--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { MdPostAdd } from "react-icons/md";
+import { useSelector } from "react-redux";
 import Posts from "../components/Posts/Posts";
 import { useLoaderData } from "react-router";
 import { getPosts } from "../services/posts";
+import AddPostModal from "../components/General/Overlays/Modals/AddPostModal";
+import styles from "./PostsPage.module.css";
 
 const PostsPage = () => {
+  const { token } = useSelector((state) => state.auth);
+  const [isAddPostModalVisible, setIsAddPostModalVisible] = useState(false);
+
+  const showAddPostModal = (event) => {
+    setIsAddPostModalVisible(true);
+  };
+  const hideAddPostModal = async (event) => {
+    setIsAddPostModalVisible(false);
+  };
+
   // Gets data from the loader defined for the current page
   const posts = useLoaderData();
 
-  return <Posts posts={posts} />;
+  return (
+    <>
+      <AddPostModal
+        isVisible={isAddPostModalVisible}
+        hideAddPostModal={hideAddPostModal}
+      />
+
+      {token && (
+        <button className={styles["button"]} onClick={showAddPostModal}>
+          <MdPostAdd size={18} />
+          Add Post
+        </button>
+      )}
+      <Posts posts={posts} />
+    </>
+  );
 };
 export default PostsPage;
 
