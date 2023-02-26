b57bdcd630afdab75ce9cbc153986c79f527fbac
chyla95 <chyla95@gmail.com>
2023-02-26 07:19:17 +0100

    Remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Posts from "./components/Posts/Posts";
-import AddPostModal from "./components/General/Overlays/Modals/AddPostModal";
 import Header from "./components/Header/Header";
-import { useState } from "react";
 
 function App() {
   return (
